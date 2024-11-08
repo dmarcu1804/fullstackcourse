87f12dcb717af73d1b9ab12dc052f076bea37204
Dragos <45339521+dmarcu1804@users.noreply.github.com>
2024-11-08 09:48:05 +0000

    completed exercise 1.5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   return(
     <>
       <h1>
-        {props.course}
+        {props.course.name}
       </h1>
     </>
   )
@@ -37,27 +37,29 @@ const Total = ( {parts} ) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course = {course} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
