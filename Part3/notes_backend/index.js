47fb5caee569a011431208edbf9fb6e419a3ff40
Dragos Marcu <45339521+dmarcu1804@users.noreply.github.com>
2025-01-23 13:44:03 +0000

    added error handling and db

--- Part3/notes_backend/index.js
+++ Part3/notes_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 app.use(express.static('dist'))
 
@@ -43,7 +45,10 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+  //response.json(notes)
 })
 
 const generateId = () => {
@@ -56,32 +61,45 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save()
+      .then(savedNote => {
+        response.json(savedNote)
+      })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    console.log('x')
-    response.status(404).end()
-  }
+  Note.findById(request.params.id)
+      .then(note => {
+        if(note){
+          response.json(note)
+        }
+        else{
+          response.status(404).end()
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        response.status(400).send({error: 'malformatted id'})
+      })
+  // const id = Number(request.params.id)
+  // const note = notes.find(note => note.id === id)
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   console.log('x')
+  //   response.status(404).end()
+  // }
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -93,7 +111,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ Part3/notes_backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length<3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+`mongodb+srv://fullstack:${password}@cluster0.uenxp.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+    content: 'testing note',
+    important: true,
+})
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
+
+// note.save().then(result => {
+//     console.log(note)
+//     console.log('note saved!')
+//     mongoose.connection.close()
+// })
