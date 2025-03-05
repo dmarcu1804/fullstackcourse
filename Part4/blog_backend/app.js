85c6eedbe83649d3ac9bb7608df2193708f66d4e
Dragos <45339521+dmarcu1804@users.noreply.github.com>
2025-03-05 17:05:37 +0000

    completed exercise 4.15 - added functionality to craete Users

--- Part4/blog_backend/app.js
+++ Part4/blog_backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const info = require('./utils/logger')
-
+const usersRouter = require('./controllers/users')
 
 info('connecting to database')
 mongoose.connect(config.mongoUrl)
@@ -13,5 +13,6 @@ mongoose.connect(config.mongoUrl)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
